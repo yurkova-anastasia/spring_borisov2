1ae45b631954843f30ff60502d130e2ef26faae5
AnastYurkova <anastasia.yurkova2003@mail.ru>
2023-01-30 20:34:22 +0300

    main

--- src/main/java/com/ya/spring2/Main.java
+++ src/main/java/com/ya/spring2/Main.java
@@ -1,7 +1,20 @@
 package com.ya.spring2;
 
+import com.ya.spring2.context.Application;
+import com.ya.spring2.context.ApplicationContext;
+import com.ya.spring2.model.CoronaDesinfector;
+import com.ya.spring2.model.Room;
+import com.ya.spring2.policeman.Policeman;
+import com.ya.spring2.policeman.impl.PolicemanImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplicationContext context
+                = Application.run("com.ya.spring2", new HashMap<>(Map.of(Policeman.class, PolicemanImpl.class)));
+        CoronaDesinfector desinfector = context.getObject(CoronaDesinfector.class);
+        desinfector.start(new Room());
     }
 }
